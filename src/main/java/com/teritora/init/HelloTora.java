c56fd8e452ac97b961f870a8a405e1be3aadfe6f
Sajjad <sajjad.bashar@gmail.com>
2018-08-09 02:32:40 -0400

    Starting Teritora git project

---
+++ src/main/java/com/teritora/init/HelloTora.java
@@ -0,0 +1,7 @@
+package com.teritora.init;
+
+public class HelloTora {
+	public static void main (String[] args) {
+		System.out.println("Teritora Project started at: 1:45am Thursday, August 9, 2018");
+	}
+}
